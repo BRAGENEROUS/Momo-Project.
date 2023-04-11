e1137d6f4fedb329cc84e1f37e6e585940d7005c
BRAGENEROUS <130256030+BRAGENEROUS@users.noreply.github.com>
2023-04-11 00:23:15 +0000

    Add files via upload

--- Momo Project.cpp
+++ Momo Project.cpp
@@ -1,128 +1,100 @@
 #include <iostream>
-
 using namespace std;
 
-int main() 
-{
-	
-	string option, old_pin,new_pin,receiver_num;
-	bool pin_out;
-	int pin_count, pin_limit,amount,total_amount;
-	total_amount=1000;
-	pin_limit=2;
-	pin_count=0;
-	pin_out=false;
-	while(true){
-		cout<<"welcome to GENEROUS mobile money service \n";
-	    cout<<"\n";
-	    cout<<"1, Authentication \n";
-	    cout<<"2, Reset pin \n";
-    	cout<<"3, Check balance \n";
-    	cout<<"4, send money \n";
-    	cout<<"5, exit \n";
-	    cout<<"";
-	    cout<<"select option by enter 1 - 5: \n";
-	    cin>>option;
-    	if (option=="1"){
-    		while(old_pin != "0000" and not(pin_out)){
-    			if (pin_count<=pin_limit){
-    				cout<<"enter your pin to accept withdrawal of 500 cedis: ";
-				    cin>>old_pin;
-				    pin_count+=1;}	
-				
-			    else{pin_out=true;}
-		     	}
-		if (pin_out==true){
-			cout<<"your account is blocked for too many atempts \n";
-			break;}
-		else{
-			cout<<"you have successfully withdrawn 500cedis from your account and your balance is 495";
-			cout<<" with fee of 5 cedis \n";
-		}
-		
-		
-	}
-	else if (option=="2"){
-		while(old_pin != "0000" and not(pin_out)){
-			if (pin_count<=pin_limit){
-				cout<<"enter old pin: ";
-				cin>>old_pin;
-				pin_count+=1;}	
-				
-			else{pin_out=true;}
-			}
-			
-			
-		if (pin_out==true){
-			cout<<"your account is blocked for too many atempts";
-			break;}   
-		else{
-		cout<<"enter new pin: ";
-		
-		cin>>new_pin;
-		if (new_pin==old_pin){
-			cout<<"pin already exist \n";
-			cout<<"\n";
-		}
-		else{
-		
-		cout<<"your pin has been resetted successfully \n";
-		cout<<"\n";
-		}}}
-	
-	else if (option=="3"){
-		while(old_pin != "0000" and not(pin_out)){
-			
-			if (pin_count<=pin_limit){
-				cout<<"enter pin: ";
-				cin>>old_pin;
-				pin_count+=1;}	
-				
-			else{pin_out=true;}
-			}
-		if (pin_out==true){
-		cout<<"your account is blocked for too many attempts \n";
-		break;
-		
-		}
-		else{
-			cout<<"your balance is 1000 cedis \n";
-			cout<<"\n";
-			
-		}
-		
-	}
-	else if (option=="4"){
-		cout<<"enter receiver number: ";
-		cin>>receiver_num;
-		cout<<"enter amount to be sent: ";
-		cin>>amount;
-		
-		while(old_pin != "0000" and not(pin_out)){
-			if (pin_count<=pin_limit){
-				cout<<"enter pin: ";
-				cin>>old_pin;
-				pin_count+=1;}	
-				
-			else{pin_out=true;}
-			}
-		if (pin_out==true){
-			cout<<"your account is blocked for too many attempts";
-			break;
-		}
-		else{
-			cout<<"you have successfully sent an amount of "<<amount<<" to "<<receiver_num<<", your balance is "<<total_amount-amount<<endl;
-			cout<<"\n";
-		}
-	}
-	else if(option=="5"){
-		cout<<"Thank you for your service with us \n ";
-		break;
-	}
-	}
-		
-		
-	
-	
-		return 0;
+int main() {
+    // Set default values
+    int balance = 1000;
+    int pin = 0;
+    int attempts = 0;
+    bool authenticated = true;
+    
+    // Main menu
+    while (true) {
+        cout << "Welcome to the MOMO system.\n";
+        cout << "1. Enter PIN\n";
+        cout << "2. Reset/Change PIN\n";
+        cout << "3. Check Balance\n";
+        cout << "4. Send Money\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        
+        int choice;
+        cin >> choice;
+        cout << endl;
+        
+        switch (choice) {
+            case 1: // Enter PIN
+                if (!authenticated) {
+                    if (attempts >= 3) {
+                        cout << "Too many attempts. Exiting...\n";
+                        return 0;
+                    }
+                    
+                    cout << "Enter your PIN: ";
+                    cin >> pin;
+                    cout << endl;
+                    
+                    if (pin == 0000) {
+                        authenticated = true;
+                        attempts = 0;
+                        cout << "Authentication successful.\n";
+                    } else {
+                        attempts++;
+                        cout << "Invalid PIN. Please try again.\n";
+                    }
+                } else {
+                    cout << "You are already authenticated.\n";
+                }
+                break;
+                
+            case 2: // Reset/Change PIN
+                if (authenticated) {
+                    cout << "Enter your new PIN: ";
+                    cin >> pin;
+                    cout << endl;
+                    
+                    cout << "PIN successfully changed.\n";
+                } else {
+                    cout << "You need to authenticate first.\n";
+                }
+                break;
+                
+            case 3: // Check Balance
+                if (authenticated) {
+                    cout << "Your balance is $" << balance << ".\n";
+                } else {
+                    cout << "You need to authenticate first.\n";
+                }
+                break;
+                
+            case 4: // Send Money
+                if (authenticated) {
+                    cout << "Enter the amount to send: ";
+                    int amount;
+                    cin >> amount;
+                    cout << endl;
+                    
+                    if (amount > balance) {
+                        cout << "Insufficient balance.\n";
+                    } else {
+                        balance -= amount;
+                        cout << "Money sent successfully.\n";
+                        cout << "Your new balance is $" << balance << ".\n";
+                    }
+                } else {
+                    cout << "You need to authenticate first.\n";
+                }
+                break;
+                
+            case 5: // Exit
+                cout << "Exiting...\n";
+                return 0;
+                
+            default:
+                cout << "Invalid choice.\n";
+        }
+        
+        cout << endl;
+    }
 }
+
